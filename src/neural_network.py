3462fb80b29061a5111eaa69d913b6b00ce6c404
marcosfelipp <mfelipe11@yahoo.com.br>
2017-09-24 18:20:38 -0300

    Perceptron implementation

--- src/neural_network.py
+++ src/neural_network.py
@@ -4,7 +4,7 @@ import csv
 
 class CSVReader:
     def __init__(self):
-        self.list = []
+        self.dataset_list = []
         self.reader()
 
     def reader(self):
@@ -12,36 +12,48 @@ class CSVReader:
         try:
             tuples_csv = csv.reader(file)
             for linha in tuples_csv:
-                self.list.append(linha)
+                self.dataset_list.append(linha)
         finally:
             file.close()
 
-    def print_tuple(self):
-        for linha in self.list:
-            print(linha[3])
+    def normatize_data(self):
+        for tuple in self.dataset_list:
+            pass
 
+    def print_tuple(self):
+        for linha in self.dataset_list:
+            print(linha)
 
 class DataAnalysis:
-    def __init__(self):
-        # Network parameters:
+    def __init__(self, total_tuples):
         self.input_tenor = None
-        self.n_hidden_1 = 10
-        self.n_hidden_2 = 5
-        self.n_input = 30
-        self.n_class = 4
+
+        # Network parameters:
+        self.n_hidden_1 = 41         # 1st layer number of features
+        self.n_input = total_tuples  # Total of datasets
+        self.n_class = 24 + 1        # Classes of atacks + 1 normal
+
         self.weights = {
             'h1': tf.Variable(tf.random_normal([self.n_input, self.n_hidden_1])),
-            'h2': tf.Variable(tf.random_normal([self.n_hidden_1, self.n_hidden_2])),
-            'out': tf.Variable(tf.random_normal([self.n_hidden_2, self.n_class]))
+            'out': tf.Variable(tf.random_normal([self.n_hidden_1, self.n_class]))
         }
 
         self.biases = {
             'b1': tf.Variable(tf.random_normal([self.n_hidden_1])),
-            'b2': tf.Variable(tf.random_normal([self.n_hidden_2])),
             'b3': tf.Variable(tf.random_normal([self.n_class]))
         }
 
         self.multilayer_perceptron(self.input_tenor,self.weights, self.biases)
+
+    def perceptron(self, input_tensor, weights, biases):
+        layer_1_multiplication = tf.matmul(input_tensor, weights['h1'])
+        layer_1_addiction = tf.add(layer_1_multiplication, biases['b1'])
+        layer_1_activate = tf.nn.relu(layer_1_addiction)
+
+        out_layer_multiplication = tf.matmul(layer_1_activate, weights['out'])
+        out_layer_addiction = out_layer_multiplication + biases['out']
+
+        return out_layer_addiction
 
     def multilayer_perceptron(self, input_tensor, weights, biases):
         layer_1_multiplication = tf.matmul(input_tensor, weights['h1'])
---
+++ tests/read_file.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+
+
+file_name_queue = tf.train.string_input_producer([,])
