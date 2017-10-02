741a7479a9a965dbce8145f83b1d5be44ffe2626
marcosfelipp <mfelipe11@yahoo.com.br>
2017-10-02 17:53:30 -0300

    testes

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,31 +1,28 @@
 import tensorflow as tf
+import zipfile
+import pandas as pn
 import csv
+
 
 class CSVReader:
     def __init__(self):
+        print ('here1')
         self.dataset_list = []
         self.reader()
 
     def reader(self):
-        file = open("../data/kddcup.data10", 'r')
-        try:
-            tuples_csv = csv.reader(file)
-            for linha in tuples_csv:
-                self.dataset_list.append(linha)
-        finally:
-           file.close()
-
-    def normatize_data(self):
-        for tuple in self.dataset_list:
-            pass
-
-    def print_tuple(self):
-        for linha in self.dataset_list:
-            print(linha)
+        with zipfile.ZipFile('../data/kddcup_data_10_percent.zip', 'r') as file_zip:
+            file = file_zip.open('kddcup_data.csv')
+            data = pn.read_table(file, sep=',')
+            print(data)
 
 class DataAnalysis:
     def __init__(self, total_tuples):
         self.input_tenor = None
+
+        self.learning_rate = 0.01
+        self.training_epochs = 1000
+        self.display_step = 56
 
         # Network parameters:
         self.n_hidden_1 = 41         # 1st layer number of features
@@ -42,11 +39,13 @@ class DataAnalysis:
             'b3': tf.Variable(tf.random_normal([self.n_class]))
         }
 
-        self.multilayer_perceptron(self.input_tenor,self.weights, self.biases)
+        self.multilayer_perceptron(self.input_tenor, self.weights, self.biases)
 
     def perceptron(self, input_tensor, weights, biases):
         layer_1_multiplication = tf.matmul(input_tensor, weights['h1'])
+
         layer_1_addiction = tf.add(layer_1_multiplication, biases['b1'])
+
         layer_1_activate = tf.nn.relu(layer_1_addiction)
 
         out_layer_multiplication = tf.matmul(layer_1_activate, weights['out'])
@@ -67,3 +66,8 @@ class DataAnalysis:
         out_layer_addiction = out_layer_multiplication + biases['out']
 
         return out_layer_addiction
+
+
+if __name__ == '__main__':
+    print('aqui')
+    reader = CSVReader()
