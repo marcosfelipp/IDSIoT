c9fdf2a0986fb057594294ee7b9f636c923ffbd6
Marcos Felipe <mfelipe11@yahoo.com.br>
2017-10-06 15:53:40 -0300

    testes

--- tests/read_file.py
+++ tests/read_file.py
@@ -15,36 +15,39 @@ display_step = 100
 n_hidden_1 = 10 # 1st layer number of neurons
 
 num_input = 41
-num_classes = 4
+num_classes = 2 # Normal and anormal
 
 # tf Graph input
-X = tf.placeholder(dtype=tf.string, shape=[num_input, 1])
+input_matrix = tf.placeholder(dtype=tf.string, shape=[num_input, 1])
+output_expected = tf.placeholder(dtype=tf.string)
 
 # Store layers weight & bias
 weights = {
-    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+    'h1' : tf.Variable(tf.random_normal([num_input, n_hidden_1])),
     'out': tf.Variable(tf.random_normal([n_hidden_1, num_classes]))
 }
 biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b1' : tf.Variable(tf.random_normal([n_hidden_1])),
     'out': tf.Variable(tf.random_normal([num_classes]))
 }
 
 # Construct model
 
-layer_1 = tf.add(tf.matmul(X, weights['h1']), biases['b1'])
+layer_1_multiplication   = tf.matmul(input_matrix, weights['h1'])
+layer_1_addition         = tf.add(layer_1_multiplication, biases['b1'])
+layer_1_activation       = tf.nn.relu(layer_1_addition)
 
-out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+out_layer_multiplication = tf.matmul(layer_1_activation, weights['out'])
+out_layer_addition       = out_layer_multiplication + biases['out']
 
 
-# Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out_layer, labels=X))
+# Compare out value with output expected:
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out_layer_addition, labels=output_expected))
 
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+# Computing gradients and apply gradients automatic:
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_op)
 
-train_op = optimizer.minimize(loss_op)
-
-correct_pred = tf.equal(tf.argmax(out_layer, 1), tf.argmax(Y, 1))
+correct_pred = tf.equal(tf.argmax(out_layer_addition, 1), tf.argmax(Y, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
