30f2bacea80b996164d09adc7059b8007ea58ae1
Marcos Felipe <mfelipe11@yahoo.com.br>
2017-10-09 18:08:00 -0300

    resultados

---
+++ results/results.py
@@ -0,0 +1,52 @@
+[INFO    - 2017-10-09 11:09:39,736] Log.info - Starting test: 0
+[INFO    - 2017-10-09 11:09:52,077] Log.info - Accurace in epoch 0 : 11326.162091
+[INFO    - 2017-10-09 11:10:04,225] Log.info - Accurace in epoch 1 : 1630.863036
+[INFO    - 2017-10-09 11:10:16,417] Log.info - Accurace in epoch 2 : 0.391543
+[INFO    - 2017-10-09 11:10:28,836] Log.info - Accurace in epoch 3 : 0.261890
+[INFO    - 2017-10-09 11:10:41,889] Log.info - Accurace in epoch 4 : 0.242008
+[INFO    - 2017-10-09 11:10:54,164] Log.info - Accurace in epoch 5 : 0.221957
+[INFO    - 2017-10-09 11:11:06,810] Log.info - Accurace in epoch 6 : 0.215937
+[INFO    - 2017-10-09 11:11:19,070] Log.info - Accurace in epoch 7 : 0.222628
+[INFO    - 2017-10-09 11:11:31,376] Log.info - Accurace in epoch 8 : 0.251973
+[INFO    - 2017-10-09 11:11:43,525] Log.info - Accurace in epoch 9 : 0.260227
+[INFO    - 2017-10-09 11:11:43,526] Log.info - Optimization finished
+[INFO    - 2017-10-09 11:11:46,148] Log.info - RESULT: 0.935853
+[INFO    - 2017-10-09 11:11:46,151] Log.info - Starting test: 1
+[INFO    - 2017-10-09 11:11:58,530] Log.info - Accurace in epoch 0 : 4034.521010
+[INFO    - 2017-10-09 11:12:10,667] Log.info - Accurace in epoch 1 : 568.670757
+[INFO    - 2017-10-09 11:12:22,812] Log.info - Accurace in epoch 2 : 0.257224
+[INFO    - 2017-10-09 11:12:34,990] Log.info - Accurace in epoch 3 : 0.216198
+[INFO    - 2017-10-09 11:12:47,147] Log.info - Accurace in epoch 4 : 0.220534
+[INFO    - 2017-10-09 11:12:59,309] Log.info - Accurace in epoch 5 : 0.227337
+[INFO    - 2017-10-09 11:13:11,409] Log.info - Accurace in epoch 6 : 0.254721
+[INFO    - 2017-10-09 11:13:23,660] Log.info - Accurace in epoch 7 : 0.209250
+[INFO    - 2017-10-09 11:13:36,001] Log.info - Accurace in epoch 8 : 0.226014
+[INFO    - 2017-10-09 11:13:48,145] Log.info - Accurace in epoch 9 : 0.209834
+[INFO    - 2017-10-09 11:13:48,146] Log.info - Optimization finished
+[INFO    - 2017-10-09 11:13:50,982] Log.info - RESULT: 0.939346
+[INFO    - 2017-10-09 11:13:50,989] Log.info - Starting test: 2
+[INFO    - 2017-10-09 11:14:03,248] Log.info - Accurace in epoch 0 : 7290.099540
+[INFO    - 2017-10-09 11:14:16,840] Log.info - Accurace in epoch 1 : 2700.308997
+[INFO    - 2017-10-09 11:14:31,461] Log.info - Accurace in epoch 2 : 4.963064
+[INFO    - 2017-10-09 11:14:45,292] Log.info - Accurace in epoch 3 : 0.692967
+[INFO    - 2017-10-09 11:14:59,111] Log.info - Accurace in epoch 4 : 0.692967
+[INFO    - 2017-10-09 11:15:11,170] Log.info - Accurace in epoch 5 : 0.692967
+[INFO    - 2017-10-09 11:15:23,213] Log.info - Accurace in epoch 6 : 0.692967
+[INFO    - 2017-10-09 11:15:36,092] Log.info - Accurace in epoch 7 : 0.692967
+[INFO    - 2017-10-09 11:15:49,204] Log.info - Accurace in epoch 8 : 0.692967
+[INFO    - 2017-10-09 11:16:01,270] Log.info - Accurace in epoch 9 : 0.692967
+[INFO    - 2017-10-09 11:16:01,272] Log.info - Optimization finished
+[INFO    - 2017-10-09 11:16:04,137] Log.info - RESULT: 0.530327
+[INFO    - 2017-10-09 11:16:04,146] Log.info - Starting test: 3
+[INFO    - 2017-10-09 11:16:16,502] Log.info - Accurace in epoch 0 : 17220.420463
+[INFO    - 2017-10-09 11:16:28,541] Log.info - Accurace in epoch 1 : 4116.697528
+[INFO    - 2017-10-09 11:16:40,655] Log.info - Accurace in epoch 2 : 4.181174
+[INFO    - 2017-10-09 11:16:52,699] Log.info - Accurace in epoch 3 : 0.191303
+[INFO    - 2017-10-09 11:17:04,771] Log.info - Accurace in epoch 4 : 1.454770
+[INFO    - 2017-10-09 11:17:16,878] Log.info - Accurace in epoch 5 : 0.673491
+[INFO    - 2017-10-09 11:17:28,980] Log.info - Accurace in epoch 6 : 0.671552
+[INFO    - 2017-10-09 11:17:40,955] Log.info - Accurace in epoch 7 : 1.152208
+[INFO    - 2017-10-09 11:17:53,091] Log.info - Accurace in epoch 8 : 0.693428
+[INFO    - 2017-10-09 11:18:05,314] Log.info - Accurace in epoch 9 : 0.693428
+[INFO    - 2017-10-09 11:18:05,314] Log.info - Optimization finished
+[INFO    - 2017-10-09 11:18:08,034] Log.info - RESULT: 0.532868
