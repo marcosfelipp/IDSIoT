bcbb1cb8f0fb86dc87832ef4b836010409200214
marcosfelipp <mfelipe11@yahoo.com.br>
2018-11-14 16:53:09 -0200

    Organizando features

--- data/organize_fatures.py
+++ data/organize_fatures.py
@@ -22,6 +22,32 @@ with open(features_attack) as attack, open(features_normal) as normal,  open(fea
     for line in normal:
         vector_features_normal.append(line)
 
+    #     Verify repited tuples:
+    _normal = {}
+    _attack = {}
+    for line in vector_features_normal:
+        if line in _normal:
+            _normal[line] +=1
+        else:
+            _normal[line] = 0
+
+    for line in vector_features_attack:
+        if line in _attack:
+            _attack[line] +=1
+        else:
+            _attack[line] = 1
+    _qtd_normal = 0
+    _qtd_attack = 0
+
+    for line in _normal:
+        _qtd_normal += _normal.get(line)
+
+    for line in _attack:
+        _qtd_attack += _attack.get(line)
+
+    print(_qtd_attack)
+    print(_qtd_normal)
+
     for i in range(qtd_attack_train):
         sorted = random.randint(0, len(vector_features_attack)-1)
         vector_features_train.append(vector_features_attack.pop(sorted))
